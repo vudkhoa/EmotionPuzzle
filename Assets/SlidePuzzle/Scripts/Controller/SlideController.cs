e33bfcf3db6d95a9c63995e2afb324a362971827
vudkhoa <vudinhkhoaowl@gmail.com>
2025-08-27 10:36:21 +0700

    Update

--- Assets/SlidePuzzle/Scripts/Controller/SlideController.cs
+++ Assets/SlidePuzzle/Scripts/Controller/SlideController.cs
@@ -569,7 +569,7 @@ public class SlideController : SingletonMono<SlideController>
     public void SpawnLevel()
     {
         curLevelId = PlayerPrefs.GetInt(Constant.LEVELID, 1);
-        curLevelId = 2;
+        curLevelId = 4;
         SetTutorial();
         SetupSavePoint();
         this.SetElementGuide();
