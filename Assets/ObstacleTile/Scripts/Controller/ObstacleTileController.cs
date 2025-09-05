10a99985511a58b6502f12d7dbb679a6ca5a01ea
vudkhoa <vudinhkhoaowl@gmail.com>
2025-09-05 17:15:23 +0700

    Update

--- Assets/ObstacleTile/Scripts/Controller/ObstacleTileController.cs
+++ Assets/ObstacleTile/Scripts/Controller/ObstacleTileController.cs
@@ -34,6 +34,7 @@ public class ObstacleTileController : SingletonMono<ObstacleTileController>
                     && IsInSave(new Vector2Int(i, j)) )
                 {
                     SlideController.Instance.obstacleTilemap.SetTile(new Vector3Int(i, j, 0), null);
+                    //LockController.Instance.RemoveLock(new Vector3Int(i, j, 0));
                 }
             }
         }
@@ -43,6 +44,7 @@ public class ObstacleTileController : SingletonMono<ObstacleTileController>
             if (IsInSave(pos))
             {
                 SlideController.Instance.obstacleTilemap.SetTile(new Vector3Int(pos.x, pos.y, 0), ObstacleTile);
+                //LockController.Instance.SetLock(new Vector3Int(pos.x, pos.y, 0));
             }
         }
     }
